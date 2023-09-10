4aa2f42cb046e3f4e129b00653026afc8edd4dd3
Artemoon <aleks13063@gmail.com>
2023-09-10 14:28:45 +0200

    practice 2

--- js/learningShit.js
+++ js/learningShit.js
@@ -1,3 +1,41 @@
+/* eslint-disable indent */
+/* eslint-disable no-constant-condition */
+'use strict';
+ 
+
+// let result = '';
+// const length = 10;
+
+// for(let i = 1; i < length; i++) {
+//     for (let j = 0; j < i; j++) {
+//         result += '*';
+//     }
+    
+//     result += '\n';
+// }
+
+
+
+// console.log(result);
+
+
+
+
+// first: for (let i = 0; i <3; i++){
+//     console.log(`First level: ${i}`);
+//     for (let j = 0; j <3; j++){
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k <3; k++){
+//             if (k=== 2) continue first; 
+//             console.log(`Third level: ${k}`);
+           
+           
+//         }
+       
+       
+//     }
+// }
+
 
 const lines = 5; // Оставляем переменную lines равной 5
 let result = ''; // Инициализируем строку для формирования фигуры
@@ -47,3 +85,14 @@ for (let i = 1; i <= lines + 1; i++) { // Увеличиваем высоту ф
 
 // Выводим результат в консоль
 console.log(result);
+
+
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j < lines - i; j++) {
+        result += ' ';
+    }
+    for (let j = 0; j < 2 * i + 1; j++) {
+        result += '*';
+    }
+    result += '\n';
+}
--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
+
 const personalMovieDB = {
     count : numberOfFilms,
     movies :{},
@@ -12,13 +13,40 @@ const personalMovieDB = {
 };
 
 
-const a = prompt('Последние просмотреные фильмы?', ''),
-      b = prompt('На сколько вы его оцените?'),
-      c = prompt('Последние просмотреные фильмы?', ''),
-      d = prompt('На сколько вы его оцените?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i =0; i < 2; i++) {
+    const a = prompt('Последние просмотреные фильмы?', '');
+    const b = prompt('На сколько вы его оцените?');
+    
+    if (a != null && b != null && a !== '' && b != '' && a.length < 50 ) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+     
+    
+    
+    
+}
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('Вы класический зритель');
+}else if (personalMovieDB.count >= 30) {
+    alert('Вы киноман!');
+
+} else {
+    alert('произошла ошибка');
+}
+
+
+
+
 
 
 console.log(personalMovieDB);
